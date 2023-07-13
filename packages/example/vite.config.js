e6e8dfb2602f4cbf7c9e88e7797581acdd97675b
xiehaijie <320974775@qq.com>
2023-07-13 14:45:26 +0800

    c

--- packages/example/vite.config.js
+++ packages/example/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import p from 'vite-plugin-multi-page-app'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
